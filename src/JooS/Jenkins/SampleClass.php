8d2dc154aa0181cd0a5703c2ffb7305226810a59
Andrey F. Mindubaev <covex.mobile@gmail.com>
2015-04-21 14:12:04 +0300

    Updated phpcs

--- src/JooS/Jenkins/SampleClass.php
+++ src/JooS/Jenkins/SampleClass.php
@@ -4,18 +4,17 @@ namespace JooS\Jenkins;
 
 class SampleClass
 {
-  
-  /**
-   * Return sum of two parameters
-   * 
-   * @param integer $first  First parameter
-   * @param integer $second Second parameter
-   * 
-   * @return integer
-   */
-  public static function add($first, $second)
-  {
-    return $first + $second;
-  }
-  
+
+    /**
+     * Return sum of two parameters
+     *
+     * @param integer $first  First parameter
+     * @param integer $second Second parameter
+     *
+     * @return integer
+     */
+    public static function add($first, $second)
+    {
+        return $first + $second;
+    }
 }
